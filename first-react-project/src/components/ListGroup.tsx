a8af016921caa01abe1289210492ba719d255461
Roddy <roddymcg787@gmail.com>
2024-06-27 19:35:50 -0700

    commit 1

--- first-react-project/src/components/ListGroup.tsx
+++ first-react-project/src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
 function ListGroup() {
+  let items = ["new yor", "san francisco", "tokyo", "paris"];
+  items = [];
+
   return (
     //<> tels react to wrap in fragment
+    //curly braces {} for dynamic stuff which isnt allowed in jsx
     <>
       <h1>list</h1>
+      {items.length === 0 && <p>no item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
